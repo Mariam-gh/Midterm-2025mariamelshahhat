3e590e31d2f563bc0a85d42957bccff084da48d8
Mariam <mariamed@umish.edu>
2025-10-23 16:37:37 -0400

    change int 6 to 7

--- factorialcalculator.java
+++ factorialcalculator.java
@@ -1,6 +1,6 @@
 public class factorialcalculator {
     public static void main(String[] args) {
-        int num =6;  // Predefined number to calculate factorial //change int from 5 to 6 in Mariam's branch
+        int num =7;  // Predefined number to calculate factorial //change int from 5 to 6 in Mariam's branch 
         int factorial = 1;
 
         for (int i = 1; i <= num; i++) { // Loop through numbers
